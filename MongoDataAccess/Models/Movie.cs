b7cdfd6c28e52102cede3569568adacde582a5cd
itsCristianPro7 <crisprox7@gmail.com>
2024-03-14 13:10:39 -0600

    Add movie CRUD.

---
+++ MongoDataAccess/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace MongoDataAccess.Models
+{
+    public class Movie
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public TimeSpan Duration { get; set; }
+    }
+}
